445d284fca7f216b0e2f78dff0079d02974a9fcf
Joel Shapiro <joeladamshapiro@gmail.com>
2022-08-27 23:15:51 -0400

    basic user interface and functions

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
+function add(a, b) {
+    let res = a + b;
+    return res;
+}
 
+console.log(add(4, 5));
+
+function subtract(a, b) {
+    let res = a - b;
+    return res;
+}
+
+console.log(subtract(4, 5));
+
+function multiply(a, b) {
+    let res = a * b;
+    return res;
+}
+
+console.log(multiply(4, 5));
+console.log(multiply(-1, 5));
+
+function divide(a, b) {
+    let res = a / b;
+    return res; 
+}
+
+console.log(divide(4, 5));
+console.log(divide(6, 2));
+
+function operate(operator, nums) {
+    let res = subtract(a, b);
+    return res;
+}
